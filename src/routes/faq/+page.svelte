c4eafbe8488efe8e6245db637c829b9315c8c8bb
TheLeeeo <leonard.e.forsberg@gmail.com>
2024-03-27 23:10:17 +0100

    add faq page

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	import FaqItem from './FaqItem.svelte';
+
+	let faqs = [
+		{
+			question: 'Blev han tappad som liten?',
+			answer:
+				'Fram tills ture fyllde 6 bodde han med en flock gätter, allt stångande har gjort honom lite vimsig.'
+		},
+		{
+			question: 'Skämtar han eller är han verkligen såhär?',
+			answer: 'Detta är den ture vi är fast med så vi får göra den bästa av situationen.'
+		},
+		{
+			question: 'Kan han spela Wonderwall?',
+			answer: 'Det är hans favoritlåt att spela. Be honom spela den nästa gång du träffar honom!'
+		},
+		{
+			question: 'Vilka är Tures favorit tår?',
+			answer: 'Andras…'
+		},
+		{
+			question: 'Hur mång laxar i en laxask?',
+			answer: '6'
+		}
+	];
+</script>
+
+<div>
+	<h1>Frequently Asked Questions</h1>
+	{#each faqs as { question, answer }}
+		<FaqItem {question} {answer} />
+	{/each}
+</div>
+
+<style>
+	h1 {
+		text-align: center;
+		color: #333;
+
+		margin: 1rem;
+	}
+
+	div {
+		/* display: flex;
+    flex-direction: column;
+    align-items: center; */
+
+		width: 80%;
+	}
+</style>
---
+++ src/routes/faq/FaqItem.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let question = '';
+	export let answer = '';
+	let isOpen = false;
+</script>
+
+<button class="faq-item" on:click={() => (isOpen = !isOpen)}>
+	<button class="question"
+		>{question}
+		<span class="toggle-icon">{isOpen ? '−' : '+'}</span>
+	</button>
+	{#if isOpen}
+		<div class="answer">{answer}</div>
+	{/if}
+</button>
+
+<style>
+	.faq-item {
+		width: 100%;
+		border-bottom: 1px solid #ccc;
+		cursor: pointer;
+	}
+	.question {
+		background: none;
+		border: none;
+		width: 100%;
+		text-align: left;
+		padding: 15px;
+		font-size: 18px;
+		font-weight: bold;
+		color: #333;
+	}
+	.toggle-icon {
+		float: right;
+	}
+	.answer {
+		padding: 15px;
+		font-size: 16px;
+		color: #666;
+		text-align: left;
+	}
+</style>
